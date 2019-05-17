8aec244ac6b528327a7de6ef0be07ff972f96fbe
Lyndon Panton <LyndonPanton@gmail.com>
2019-05-17 21:43:33 +0100

    Added form validation

--- js/app.js
+++ js/app.js
@@ -20,14 +20,20 @@ const b = new Contact("bob", "bobson", 21, 23456789012, "bob2@mail.com", "2 Some
 const form = document.getElementById("form-field");
 form.addEventListener("submit", function(event) {
 	event.preventDefault();
-	let errorContainer = document.createElement("div");
 
-	// errorContainer.appendChild(validateFirstName(this.children[0].children[1]));
-	// errorContainer.appendChild(validatesurname(this.children[1].children[1]));
-	// errorContainer.appendChild(validateAge(this.children[2].children[1]));
-	// errorContainer.appendChild(validateNumber(this.children[3].children[1]));
-	// errorContainer.appendChild(validateEmail(this.children[4].children[1]));
-	// errorContainer.appendChild(validateAddress(this.children[5].children[1]));
+	let fe = document.getElementById("form-error");
+	if (fe) {
+		fe.parentElement.removeChild(fe);
+	}
+	let errorContainer = document.createElement("div");
+	errorContainer.id = "form-error";
+
+	errorContainer.appendChild(validateFirstName(this.children[0].children[1].value));
+	errorContainer.appendChild(validateSurname(this.children[1].children[1].value));
+	errorContainer.appendChild(validateAge(this.children[2].children[1].value));
+	errorContainer.appendChild(validateNumber(this.children[3].children[1].value));
+	errorContainer.appendChild(validateEmail(this.children[4].children[1].value));
+	errorContainer.appendChild(validateAddress(this.children[5].children[1].value));
 
 	if (errorContainer.textContent) {
 		// Add error container
@@ -46,26 +52,98 @@ form.addEventListener("submit", function(event) {
 	}
 });
 
-function validateFirstName(fName) {
-	// if (fName )
+function validateFirstName(fn) {
+	let error = document.createElement("p");
+
+	if (fn.trim() == "") {
+		error.textContent = "First name should not be empty";
+	} else if (/\s/.test(fn)) {
+		error.textContent = "First name should not contain white space";
+	} else if (fn.trim().length < 3) {
+		error.textContent = "First name should be at least 3 characters";
+	} else if (/[^a-zA-Z-]/.test(fn)) {
+		error.textContent = "First name should only include latin letters or hyphens";
+	}
+
+	return error;
 }
 
-function validateSurname(sName) {
+function validateSurname(s) {
+	let error = document.createElement("p");
 
+	if (s.trim() == "") {
+		error.textContent = "Surname should not be empty";
+	} else if (/\s/.test(s)) {
+		error.textContent = "Surname should not contain white space";
+	} else if (s.trim().length < 3) {
+		error.textContent = "Surname should be at least 3 characters";
+	} else if (/[^a-zA-Z-]/.test(s)) {
+		error.textContent = "Surname should only include latin letters or hyphens";
+	}
+
+	return error;
 }
 
-function validateAge(age) {
+function validateAge(a) {
+	let error = document.createElement("p");
 
+	if (a.trim() == "") {
+		error.textContent = "Age should not be empty";
+	} else if (/\s/.test(a)) {
+		error.textContent = "Age should not contain white space";
+	} else if (/[^\d]/.test(a)) {
+		error.textContent = "Age should only contain numbers";
+	} else if (Number(a) > 199) {
+		error.textContent = "Age should be less than 200";
+	}
+
+	return error;
 }
 
-function validateNumber(number) {
+function validateNumber(n) {
+	let error = document.createElement("p");
 
+	if (n.trim() == "") {
+		error.textContent = "Number should not be empty";
+	} else if (/\s/.test(n)) {
+		error.textContent = "Number should not contain white space";
+	} else if (/\D/.test(n)) {
+		error.textContent = "Number should only contain numbers";
+	} else if (n.trim().length < 10) {
+		error.textContent = "Number should not be less than 10 digits";
+	} else if (n.trim().length > 15) {
+		error.textContent = "Number should not be more than 15 digits";
+	}
+
+	return error;
 }
 
-function validateEmail(email) {
+function validateEmail(e) {
+	let error = document.createElement("p");
 
+	if (e.trim() == "") {
+		error.textContent = "Email should not be empty";
+	} else if (/\s/.test(e)) {
+		error.textContent = "Email should not contain white space";
+	} else if (0) { // Create email validation
+		error.textContent = "Email is not valid";
+	}
+
+	return error;
 }
 
-function validateAddress(address) {
+function validateAddress(a) {
+	let error = document.createElement("p");
 
+	if (a.trim() == "") {
+		error.textContent = "Address should not be empty"
+	} else if (/[^\a-zA-Z0-9\s]/.test(a)) {
+		error.textContent = "Address should only contain letters and numbers";
+	} else if (!/[a-zA-Z]/.test(a) && !/[0-9]/.test(a)) {
+		error.textConent = "Address should contain at least one letter and one number";
+	} else if (a.trim().length > 20) {
+		error.textContent = "Address should be less than 20 characters";
+	}
+
+	return error;
 }
